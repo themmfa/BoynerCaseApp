af5f08c647d4d403c2091001ee7ed2423c4dc229
FE <fatiherdgn25@gmail.com>
2026-04-14 18:09:48 +0300

    add project root folders and api key storage

---
+++ BoynerCaseApp/BoynerCaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoynerCaseAppApp.swift
+//  BoynerCaseApp
+//
+//  Created by F E on 14.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct BoynerCaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SourcesView()
+        }
+    }
+}
